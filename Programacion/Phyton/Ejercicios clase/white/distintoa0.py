c25b495d2a2a296c582c3a5f3fa62f2b4cd67d1e
juaam97 <jmagmej503@g.educaand.es>
2025-10-07 10:09:08 +0200

    07/10/25

---
+++ Programacion/Phyton/Ejercicios clase/white/distintoa0.py
@@ -0,0 +1,8 @@
+edad = int(input("Que edad tienes?: "))
+
+while edad > 0 :
+    match edad :
+        case 18 :
+            print("Dispones del Bono cultural")
+    edad = int(input("Que edad tienes?: "))
+print ("FIN")
